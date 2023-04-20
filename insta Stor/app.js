fb57dba12008ca3c801779efed29ee5f468fdbd0
Hamza Tahiani <67624408+Hamzatahiani724@users.noreply.github.com>
2023-04-20 06:44:23 +0000

    Add files via upload

---
+++ insta Stor/app.js
@@ -0,0 +1,17 @@
+inta = document.querySelector(".one")
+inta.addEventListener("click",function URL() {
+    window.replace(href='../pages/insta.html') ;
+})
+intaa = document.querySelector(".insta")
+intaa.addEventListener("click",function URL() {
+    location.reload(href='../pages/insta.html','none') ;
+})
+ianta = document.getElementById("vv")
+ianta.addEventListener("click",function URL() {
+    window.replace(href='../index.html') ;
+    console.log(ianta)
+})
+// inta = document.querySelector(".one")
+// inta.addEventListener("click",function URL() {
+//     window.open('https://wa.me/message/N2FRWI675TSYL1','_blank') ;
+// })
---
+++ insta Stor/instaapp.js
@@ -0,0 +1,6 @@
+
+ianta = document.querySelector(".vv")
+ianta.addEventListener("click",function URL() {
+    window.replce(href='./') ;
+    console.log(ianta)
+})
